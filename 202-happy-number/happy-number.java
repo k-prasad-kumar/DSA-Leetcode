f82439c33ba0d1072aa436c1bf3c89d9ceec9de3
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-06-14 22:04:06 +0530

    Time: 1 ms (68.12%) | Memory: 41.8 MB (5.15%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int squareOfNum(int n){
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        while(n != 0){
+            int digit = n%10;
+            nums.add(digit);
+            n = n/10;
+        }
+        int ans = 0;
+        for(int num: nums){
+            ans += num*num;
+        }
+
+        return ans;
+    }
+
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+        do{
+            slow = squareOfNum(slow);
+            fast = squareOfNum(squareOfNum(fast));
+        }while(slow != fast);
+
+        return slow == 1;
+    }
+}
