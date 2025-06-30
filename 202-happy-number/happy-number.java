7b03d6c9bdc110198bb0acef5be15b901742045f
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-06-30 16:29:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (52.02%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,29 +1,40 @@
 class Solution {
-    public int squareOfNum(int n){
-        ArrayList<Integer> nums = new ArrayList<>();
+    // public int squareOfNum(int n){
+    //     ArrayList<Integer> nums = new ArrayList<>();
 
-        while(n != 0){
-            int digit = n%10;
-            nums.add(digit);
-            n = n/10;
-        }
-        int ans = 0;
-        for(int num: nums){
-            ans += num*num;
-        }
+    //     while(n != 0){
+    //         int digit = n%10;
+    //         nums.add(digit);
+    //         n = n/10;
+    //     }
+    //     int ans = 0;
+    //     for(int num: nums){
+    //         ans += num*num;
+    //     }
 
-        return ans;
-    }
+    //     return ans;
+    // }
 
     public boolean isHappy(int n) {
 
-        int slow = n;
-        int fast = n;
-        do{
-            slow = squareOfNum(slow);
-            fast = squareOfNum(squareOfNum(fast));
-        }while(slow != fast);
+        // int slow = n;
+        // int fast = n;
+        // do{
+        //     slow = squareOfNum(slow);
+        //     fast = squareOfNum(squareOfNum(fast));
+        // }while(slow != fast);
 
-        return slow == 1 || slow == 7;
+        // return slow == 1 || slow == 7;
+        int sum = 0, rem;
+        while(n > 9){
+            while(n!=0)
+            {
+                rem = n % 10;
+                sum += rem * rem;
+                n /= 10; 
+            }
+            n = sum; sum = 0;
+        }
+        return n == 1 || n == 7;
     }
 }
