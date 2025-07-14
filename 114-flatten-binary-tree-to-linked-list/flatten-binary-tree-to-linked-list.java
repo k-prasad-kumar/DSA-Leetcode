0bd1e59d6f621aafc4fe93cec86b8b8dad0297bf
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-07-14 17:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (99.63%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -19,9 +19,11 @@ class Solution {
     public void flatten(TreeNode root) {
         if (root == null) return;
 
+        // first traverse right cause right side last node is the last node of the faltten LL
         flatten(root.right);
         flatten(root.left);
 
+        // when we backtrack we add curr node as prev node's right & null as left
         root.right = prev;
         root.left = null;
         prev = root;
