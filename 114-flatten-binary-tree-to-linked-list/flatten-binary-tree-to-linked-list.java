c3c8446a6097008699a2b483d164d8d964c1f863
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-07-16 21:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.66%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -19,13 +19,37 @@ class Solution {
     public void flatten(TreeNode root) {
         if (root == null) return;
 
-        // first traverse right cause right side last node is the last node of the faltten LL
-        flatten(root.right);
-        flatten(root.left);
+        // using RC
+        // // first traverse right cause right side last node is the last node of the faltten LL
+        // flatten(root.right);
+        // flatten(root.left);
 
-        // when we backtrack we add curr node as prev node's right & null as left
-        root.right = prev;
-        root.left = null;
-        prev = root;
+        // // when we backtrack we add prev node as curr node's right & null as left
+        // root.right = prev;
+        // root.left = null;
+        // prev = root;
+
+        // using Morris Traversal
+        TreeNode curr = root;
+
+        while(curr != null){
+             // If left child is not null
+            if(curr.left != null){
+                // Find the inorder predecessor of curr
+                TreeNode prev = curr.left;
+                while(prev.right != null){
+                    prev = prev.right;
+                }
+                // point predecessor right to curr right (curr is root first time)
+                // poit curr right to curr left
+                // point curr left to null
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            // traverse untill curr left is null 
+            // it will reach end node so loop will break
+            curr = curr.right;
+        }
     }
 }
