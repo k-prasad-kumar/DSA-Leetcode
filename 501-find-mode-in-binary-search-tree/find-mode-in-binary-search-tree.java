24328c4569f7f2532b8dfe1245e63ccfc7f34cce
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-07-23 21:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (69.11%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -14,37 +14,61 @@
  * }
  */
 class Solution {
-    private void inorder(TreeNode root, HashMap<Integer, Integer> map){
+    List<Integer> modes = new ArrayList<>();
+    TreeNode prev = null;
+    int currentCount = 0;
+    int maxCount = 0;
+
+    private void inorder(TreeNode root){
         if(root == null) return;
 
-        inorder(root.left, map);
+        inorder(root.left);
+        // if(map.containsKey(root.val)){
+        //     map.put(root.val, map.get(root.val)+1);
+        // }else{
+        //     map.put(root.val, 1);
+        // }
 
-        if(map.containsKey(root.val)){
-            map.put(root.val, map.get(root.val)+1);
+        if(prev != null && prev.val == root.val){
+            currentCount++;
         }else{
-            map.put(root.val, 1);
+            currentCount = 1;
+        }
+        
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            modes = new ArrayList<>();
+            modes.add(root.val);
+        } else if (currentCount == maxCount) {
+            modes.add(root.val);
         }
 
-        inorder(root.right, map);
+        prev = root;
+
+        inorder(root.right);
     }
+
     public int[] findMode(TreeNode root) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        inorder(root, map);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // inorder(root, map);
 
-        // get max 
-        int max = 0;
-        for(int i: map.keySet()) if(max < map.get(i)) max = map.get(i);
+        // // get max 
+        // int max = 0;
+        // for(int i: map.keySet()) if(max < map.get(i)) max = map.get(i);
 
-        // store all max values into arraylist
-        List<Integer> al = new ArrayList<>();
-        for(int i: map.keySet()){
-            if(map.get(i) == max) al.add(i);
-        }
+        // // store all max values into arraylist
+        // List<Integer> al = new ArrayList<>();
+        // for(int i: map.keySet()){
+        //     if(map.get(i) == max) al.add(i);
+        // }
+
+        
+        inorder(root);
 
         // convert arraylist into array
-        int[] arr = new int[al.size()];
-        for(int i=0; i<al.size(); i++){
-            arr[i] = al.get(i);
+        int[] arr = new int[modes.size()];
+        for(int i=0; i<modes.size(); i++){
+            arr[i] = modes.get(i);
         }
         return arr;
     }
