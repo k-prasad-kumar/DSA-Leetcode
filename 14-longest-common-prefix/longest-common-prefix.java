57b94b287451502a5c5d57aa22c0f6da2bd1f324
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-09-20 19:57:15 +0530

    Time: 3 ms (14.48%) | Memory: 44.7 MB (5.36%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,24 +1,106 @@
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        String ans = "";
+class TrieNode{
+	char data;
+	TrieNode[] children = new TrieNode[26];
+	int childCount;
+	boolean isTerminal;
 
-        if(strs.length == 0) return ans;
+	TrieNode(char ch){
+		data = ch;
 
-        for(int i=0; i<strs[0].length(); i++){
-            char ch = strs[0].charAt(i);
-            boolean match = true;
+		for(int i=0; i<26; i++){
+			children[i] = null;
+		}
+		childCount = 0;
+		isTerminal = false;
+	}
+}
 
-            for(int j=1; j<strs.length; j++){
-                if(strs[j].length() <= i || strs[j].charAt(i) != ch) {
-                    match = false;
-                    break;
-                }
-            }
+class Trie{
+	TrieNode root;
 
-            if(match == true) ans += "" + ch;
-            else break;
+	Trie(char ch){
+		root = new TrieNode(ch);
+	}
+
+    void insertUtil(TrieNode root, String word){
+        // base case
+        if(word.length() == 0) {
+            root.isTerminal = true;
+            return;
         }
 
-        return ans;
+        int index = word.charAt(0) - 'a';
+        TrieNode child;
+        // if present
+        if(root.children[index] != null){
+            child = root.children[index];
+        }else { // absent
+            child = new TrieNode(word.charAt(0));
+            root.children[index] = child;
+            root.childCount++;
+        }
+
+        // recursion
+        insertUtil(child, word.substring(1));
+    }
+
+    void insertWord(String word){
+        insertUtil(root, word);
+    }
+
+    void lcp(String word, StringBuilder ans){
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+
+            if(root.childCount == 1){
+                ans.append(ch);
+
+                int index = ch - 'a';
+                root = root.children[index];
+            }else {
+                break;
+            }
+
+            if(root.isTerminal) break;
+        }
+    }
+}
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder ans = new StringBuilder();
+        if(strs.length == 0) return ans.toString();
+
+        // // 1st approach
+
+        // for(int i=0; i<strs[0].length(); i++){
+        //     char ch = strs[0].charAt(i);
+        //     boolean match = true;
+
+        //     for(int j=1; j<strs.length; j++){
+        //         if(strs[j].length() <= i || strs[j].charAt(i) != ch) {
+        //             match = false;
+        //             break;
+        //         }
+        //     }
+
+        //     if(match == true) ans.append(ch);
+        //     else break;
+        // }
+
+        // 2nd approach
+        Trie t = new Trie('\0');
+
+        // insert all words into trie
+        for(String str: strs) {
+            if(str.length() == 0) return ans.toString();
+            t.insertWord(str);
+        }
+
+        t.lcp(strs[0], ans);
+
+        return ans.toString();
+
+
     }
 }
