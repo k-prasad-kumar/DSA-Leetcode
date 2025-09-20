436e13de12a35e0c7f946db9e05b0b2074e651e5
Prasad Kumar <140512061+k-prasad-kumar@users.noreply.github.com>
2025-09-20 20:20:00 +0530

    Time: 40 ms (31.87%) | Memory: 55.5 MB (45.83%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,105 @@
+
+class TrieNode{
+	char data;
+	TrieNode[] children = new TrieNode[26];
+	int childCount;
+	boolean isTerminal;
+
+	TrieNode(char ch){
+		data = ch;
+
+		for(int i=0; i<26; i++){
+			children[i] = null;
+		}
+		childCount = 0;
+		isTerminal = false;
+	}
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode('\0');
+    }
+    
+    public void insertWord(TrieNode root, String word){
+        // base case
+        if(word.length() == 0) {
+            root.isTerminal = true;
+            return;
+        }
+
+        int index = word.charAt(0) - 'a';
+        TrieNode child;
+        // if present
+        if(root.children[index] != null){
+            child = root.children[index];
+        }else { // absent
+            child = new TrieNode(word.charAt(0));
+            root.children[index] = child;
+            root.childCount++;
+        }
+
+        // recursion
+        insertWord(child, word.substring(1));
+    }
+
+    public void insert(String word) {
+        insertWord(root, word);
+    }
+
+    public boolean searchWord(TrieNode root, String word){
+        // base case
+        if(word.length() == 0) {
+            return root.isTerminal;
+        }
+
+        int index = word.charAt(0) - 'a';
+        
+        // if present
+        if(root.children[index] != null){
+            root = root.children[index];
+        }else { // absent
+            return false;
+        }
+
+        // recursion
+        return searchWord(root, word.substring(1));
+    }
+    
+    public boolean search(String word) {
+        return searchWord(root, word);
+    }
+    
+    public boolean prefixUtil(TrieNode root, String word){
+        // base case
+        if(word.length() == 0) {
+            return true;
+        }
+
+        int index = word.charAt(0) - 'a';
+    
+        // if present
+        if(root.children[index] != null){
+            root = root.children[index];
+        }else { // absent
+            return false;
+        }
+
+        // recursion
+        return prefixUtil(root, word.substring(1));
+    }
+
+    public boolean startsWith(String prefix) {
+        return prefixUtil(root, prefix);
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
